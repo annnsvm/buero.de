133576c57fdb295c3b980fd1e821841bfae8d7fa
ZorAnderius <gabriel.zorander@gmail.com>
2026-03-19 13:42:17 +0000

    fix: routes redirecrion for succes and unsuccess payment

--- buero-frontend/src/pages/MyLearningPage/MyLearningPage.tsx
+++ buero-frontend/src/pages/MyLearningPage/MyLearningPage.tsx
@@ -1,9 +1,87 @@
+import { CoursesCatalogList } from '@/features/courses-catalog';
 import React from 'react'
 
+const mockCourses = [
+  {
+    id: '1',
+    title: 'German A1 – Foundations',
+    category: 'Language',
+    levelLabel: 'A1',
+    badge: 'Free trial',
+    imageUrl: '/images/courses/course-1.webp',
+    description:
+      'Start your German journey from zero. Build a solid base in pronunciation, grammar, and essential vocab.',
+    price: '€69',
+    lessonsCount: 32,
+    durationHours: 24,
+    tags: ['Beginner', 'Grammar', 'Vocabulary'],
+    rating: 4.9,
+  },
+  {
+    id: '2',
+    title: 'German A2 – Foundations',
+    category: 'Language',
+    levelLabel: 'A1',
+    badge: 'Free trial',
+    imageUrl: '/images/courses/course-1.webp',
+    description:
+      'Start your German journey from zero. Build a solid base in pronunciation, grammar, and essential vocab.',
+    price: '$55',
+    lessonsCount: 12,
+    durationHours: 6,
+    tags: ['Grammar', 'Vocabulary'],
+    rating: 4.8,
+  },
+  {
+    id: '3',
+    title: 'German B1 – Foundations',
+    category: 'Language',
+    levelLabel: 'A1',
+    badge: 'Free trial',
+    imageUrl: '/images/courses/course-1.webp',
+    description:
+      'Start your German journey from zero. Build a solid base in pronunciation, grammar, and essential vocab.',
+    price: '$55',
+    lessonsCount: 12,
+    durationHours: 6,
+    tags: ['Grammar', 'Vocabulary'],
+    rating: 4.8,
+  },
+  {
+    id: '4',
+    title: 'German A1 – Foundations',
+    category: 'Language',
+    levelLabel: 'A1',
+    badge: 'Free trial',
+    imageUrl: '/images/courses/course-1.webp',
+    description:
+      'Start your German journey from zero. Build a solid base in pronunciation, grammar, and essential vocab.',
+    price: '€69',
+    lessonsCount: 32,
+    durationHours: 24,
+    tags: ['Beginner', 'Grammar', 'Vocabulary'],
+    rating: 4.9,
+  },
+  {
+    id: '5',
+    title: 'German A1 – Foundations',
+    category: 'Language',
+    levelLabel: 'A1',
+    badge: 'Free trial',
+    imageUrl: '/images/courses/course-1.webp',
+    description:
+      'Start your German journey from zero. Build a solid base in pronunciation, grammar, and essential vocab.',
+    price: '€69',
+    lessonsCount: 32,
+    durationHours: 24,
+    tags: ['Beginner', 'Grammar', 'Vocabulary'],
+    rating: 4.9,
+  },
+];
+
+
 const MyLearningPage: React.FC = () => {
-  return (
-    <div>MyLearningPage</div>
-  )
+  return <CoursesCatalogList courses={mockCourses} />;
 }
 
 export default MyLearningPage
